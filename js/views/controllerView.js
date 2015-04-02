576599c06d563aee1856706141aa12a062c5aa3b
Yousef Al-Hadhrami <yhadhr@gmail.com>
2015-04-02 19:48:22 +0300

    fixed storage

--- js/views/controllerView.js
+++ js/views/controllerView.js
@@ -22,18 +22,25 @@ app.ControllerView = Backbone.View.extend({
 
 		$.each(tile, function( index, value ){
 			if(tile[index].get('tier') == 1){
-				main.set('stone', main.get('stone') + tile[index].get('produce_stone') );
-				main.set('wood', main.get('wood') + tile[index].get('produce_wood') );
+				if(main.get('storage') <= main.get('max_storage')){
+					main.set('stone', main.get('stone') + tile[index].get('produce_stone') );
+					main.set('wood', main.get('wood') + tile[index].get('produce_wood') );
+				}
 			}
 		});
 		
 		$.each(tile, function( index, value ){
 			if(tile[index].get('tier') == 2){
-				if(main.get('wood') >= tile[index].get('require_wood')){
-					main.set('planks', main.get('planks') + tile[index].get('produce_planks') );
+				if(main.get('wood') <= tile[index].get('require_wood')){
+					if(main.get('storage') >= main.get('max_storage')){
+						main.set('wood', main.get('wood') - tile[index].get('require_wood') );
+						main.set('planks', main.get('planks') + tile[index].get('produce_planks') );
+					}
 				}
 			}
 		});
+		
+		main.set('storage' , main.get('stone') +  main.get('wood') + main.get('planks'));
 		
 		mainView.render();
 		//main.set('stone' ,main.get('day')  )
